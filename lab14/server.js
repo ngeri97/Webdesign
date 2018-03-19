0547894ad92c47b7ef40173252526c43ce65f0b3
1607223 <ngeri97@mail.com>
2018-03-19 17:00:57 +0000

    catcat

--- lab14/server.js
+++ lab14/server.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var app = express();
- app.use(express.static('public'))
- app.get('/', function(req, res){
+var Twitter = require('twitter');
+
+var client = new Twitter({
+ consumer_key: 'agLMjcDXacKsXu1fXd4AdxDn8',
+ consumer_secret: '6zHzwOstoAPwgBrGRMBtZdgRSiQq5l828aOMCj3wJGc3T1TPxQ',
+ access_token_key: '975768435522179074-uJb3KQe2lMYIuciEpaRdIdPyKVOStj8',
+ access_token_secret: 'q2V77slhFzB0eZsWMqok0AoIAo4lubqTtjEI6w6rWSPtS'
+});
+
+app.use(express.static('public'))
+app.get('/', function(req, res){
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+   if (!error) {
+   console.log(tweets);
+   }
+  });
  res.send("Hello world! by express");
 });
 app.listen(8080);
